4854d0508378f9fb9b35ed3c679ec6c12102422f
Caelum Dudek <caelumdudek25@gmail.com>
2024-03-10 18:21:16 -0700

    Nothing is working

--- godot/Scripts/net.cs
+++ godot/Scripts/net.cs
@@ -1,17 +1,17 @@
 using Godot;
 using System;
 
-public partial class game : Node2D
+public partial class net : Node
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Console.WriteLine("Hello, world!");
+		GD.Print("Hello, world!");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Console.WriteLine("Hello, world! Process");
+		
 	}
 }

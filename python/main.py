848b4c128b2f5b2d23f46b217599941c8692b824
Doug Cruz <dcruz@uvic.ca>
2024-03-20 11:32:17 -0700

    Last working version wed march 20. changed WRITE_READ to RREAD_WRITE and messed around with the python

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import ipc
+import time
 
-JSON_LOCATION = "C:\\Users\\caelu\GodotProjects\\networked_pacman\\IPC.json"
+JSON_LOCATION = "../IPC.json"
 
 
 def process():
@@ -8,15 +9,20 @@ def process():
         ### AI PROCESSING STUFF
         data = ipc.read_game_state(JSON_LOCATION)
         print(data)
+        
+        ##PICK NEXT MOVE
+
         ### AI SENDING STUFF
         ipc.move_players(JSON_LOCATION, "Left")
         ipc.swap_process(JSON_LOCATION)
         print("*"*20)
         print("Changed")
         print("*" * 20)
-        exit()
+        # exit()
     else:
         print("Not changed")
+    
+    time.sleep(1)
 
 
 if __name__ == "__main__":

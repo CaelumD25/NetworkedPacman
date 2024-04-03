5b4f0e0c2d2f6f7b54ece4ff72ff21d7eb34a4c1
Doug Cruz <dcruz@uvic.ca>
2024-04-03 15:35:15 -0700

    Cleaned up file

--- python/graph.py
+++ python/graph.py
@@ -11,10 +11,15 @@ class Node:
 def is_accessible(maze, row, col):
     return 0 <= row < len(maze) and 0 <= col < len(maze[0]) and maze[row][col] != 1
 
-def create_graph(maze):
-    
+def create_graph(maze):   
     # documentation for this https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.shortest_paths.astar.astar_path.html
-    # has A* search already
+    
+    # Each node has 3 properties: tile_id, row, and col. I beleive each node can also be referenced using by (row,col)
+    # tile_ID:
+    #    0 = free space
+    #    1 = Wall
+    #    2 = pellet
+
     G = nx.Graph()
 
     # Iterate through the maze to add nodes
@@ -27,9 +32,34 @@ def create_graph(maze):
                 neighbor_row, neighbor_col = row + d_row, col + d_col
                 if (0 <= neighbor_row < len(maze) and  # Row in bounds
                     0 <= neighbor_col < len(maze[0])):  # Col in bounds
+                        
+                        # This part will create the adjacent node if it has not been created but properties will
+                        # not get added/updated till add_node is called for adjecent node
                         G.add_edge((row, col), (neighbor_row, neighbor_col))
     return G
-  
+
+def print_graph(G):
+    print("Nodes:", list(G.nodes(data=True)))
+    print("Edges:", list(G.edges()))
+
+def show_graph(G):
+    plt.figure(figsize=(20, 8))
+
+    pos = {(row, col): (col, -row) for row, col in G.nodes()}  # Position nodes based on their (row, col) to preserve shape
+    values = [G.nodes[node]['tile_id'] for node in G.nodes()]  # Get values for coloring
+
+    # Prepare a color map, black for 0's (Free space), blue for 1's (wall), and yellow for 2's (pellet)
+    color_map = []
+    for node in G.nodes(data=True):
+        if node[1]['tile_id'] == 0:    # Empty spot
+            color_map.append('grey')
+        elif node[1]['tile_id'] == 1:  # Wall
+            color_map.append('pink')
+        else:                          # pellet
+            color_map.append('yellow')  
+
+    nx.draw(G, pos, with_labels=True, node_color=color_map, cmap=plt.cm.Wistia, node_size=350, font_size=5)
+    plt.show()
 
 def main():
     maze = [
@@ -65,36 +95,10 @@ def main():
         [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ]
-
     
     G = create_graph(maze)
-
     print_graph(G)
-
     show_graph(G)
-
-def print_graph(G):
-    print("Nodes:", list(G.nodes(data=True)))
-    print("Edges:", list(G.edges()))
-
-def show_graph(G):
-    plt.figure(figsize=(20, 8))
-
-    pos = {(row, col): (col, -row) for row, col in G.nodes()}  # Position nodes based on their (row, col) to preserve shape
-    values = [G.nodes[node]['tile_id'] for node in G.nodes()]  # Get values for coloring
-
-    # Prepare a color map, black for 0's (Free space), blue for 1's (wall), and yellow for 2's (pellet)
-    color_map = []
-    for node in G.nodes(data=True):
-        if node[1]['tile_id'] == 0:  # Empty spot
-            color_map.append('grey')
-        elif node[1]['tile_id'] == 1:  # Wall
-            color_map.append('pink')
-        else:  # For coins or anything else you might have
-            color_map.append('yellow')  # Change 'yellow' to whatever color you like for '2's
-
-    nx.draw(G, pos, with_labels=True, node_color=color_map, cmap=plt.cm.Wistia, node_size=100, font_size=6)
-    plt.show()
 
 if __name__ == "__main__":
     main()

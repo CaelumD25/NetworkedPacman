61cc2988941ec5a4ce23babf98eaaa72a41e2066
Doug Cruz <dcruz@uvic.ca>
2024-04-03 01:06:34 -0700

    Got a decent and apparently efficient library to do the grapg conversion. It already has A* built in.

--- python/graph.py
+++ python/graph.py
@@ -1,3 +1,6 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Node:
     def __init__(self, tile_id: int, row, col):
         self.tile_id = tile_id
@@ -5,9 +8,122 @@ class Node:
         self.col = col
         self.neighbors = []
 
+def is_accessible(maze, row, col):
+    return 0 <= row < len(maze) and 0 <= col < len(maze[0]) and maze[row][col] != 1
+
 def create_graph(maze):
     #TODO Create a object representation of the response
-    pass
+    
+    # documentation for this https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.shortest_paths.astar.astar_path.html
+    # has a* search already
+
+
+    G = nx.Graph()
+
+    # Iterate through the maze to add nodes
+    for row in range(len(maze)):
+        for col in range(len(maze[row])):
+            G.add_node((row, col), value=maze[row][col])  # Add the cell as a node
+
+            # If the left neighbor is accessible, add an edge
+            if is_accessible(maze, row, col-1):
+                G.add_edge((row, col), (row, col-1))
+            
+            # If the upper neighbor is accessible, add an edge
+            if is_accessible(maze, row-1, col):
+                G.add_edge((row, col), (row-1, col))
+
+    # Optional: Print the graph's nodes and edges to verify
+    print("Nodes:", list(G.nodes(data=True)))
+    print("Edges:", list(G.edges()))
+
+    plt.figure(figsize=(20, 8))
+
+    pos = {(row, col): (col, -row) for row, col in G.nodes()}  # Position nodes based on their (row, col) to preserve shape
+    values = [G.nodes[node]['value'] for node in G.nodes()]  # Get values for coloring
+
+    # Prepare a color map, black for 0's, blue for 1's, and you can choose for 2's
+    color_map = []
+    for node in G.nodes(data=True):
+        if node[1]['value'] == 0:  # Empty spot
+            color_map.append('grey')
+        elif node[1]['value'] == 1:  # Wall
+            color_map.append('pink')
+        else:  # For coins or anything else you might have
+            color_map.append('yellow')  # Change 'yellow' to whatever color you like for '2's
+
+    nx.draw(G, pos, with_labels=True, node_color=color_map, cmap=plt.cm.Wistia, node_size=600, font_size=6)
+    plt.show()
+    # G = nx.Graph()
+    # for r, row in enumerate(maze):
+    #     for c, val in enumerate(row):
+    #         node = (r, c)
+    #         G.add_node(node)
+
+    #         # Check and connect to previous row and column nodes if they're not walls
+    #         if r > 0 and maze[r-1][c] != 1:
+    #             G.add_edge((r-1, c), node)
+    #         if c > 0 and maze[r][c-1] != 1:
+    #             G.add_edge((r, c-1), node)
+
+    # # Optional: Print the graph
+    # print("Nodes:", G.nodes(data=True))
+    # print("Edges:", G.edges())
+    
+    # # rows, cols = len(maze), len(maze[0])
+    # # G = nx.Graph()
+    # # G.add_nodes_from(maze)
+    # nx.draw_planar(G)
+    # plt.show()
+    # print("Finished create_graph")
+
+    # for x in range(rows):
+    #     for y in range(cols):
+    #         new_node = Node(tile_id = maze[x, y], row = x, col = y)
+    #         G.add_node(new_node)
+            
+                
+
+def main():
+    maze = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 1, 0, 0, 1, 2, 1, 0, 0, 0, 1, 2, 1, 1, 2, 1, 0, 0, 0, 1, 2, 1, 0, 0, 1, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1],
+        [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 1],
+        [1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1],
+        [1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1],
+        [1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1],
+        [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+        [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ]
+
+    
+    create_graph(maze)
 
 def print_graph(graph):
     print(graph)
+
+if __name__ == "__main__":
+    main()
